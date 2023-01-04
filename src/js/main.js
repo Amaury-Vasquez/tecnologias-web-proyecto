d6a7d39e1abdbea0b32e25bba1299fd8a8b1d448
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-01-04 07:49:27 -0600

    handle fallback

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
 import '../scss/style.scss';
+import { routes } from './router';
 import { createHomePage } from './home';
 import { createHeader } from './header';
 
 try {
   const root = document.querySelector('#root');
+  const { pathname } = window.location;
+  const path = pathname.split('/')[1];
+  const index = routes.findIndex((route) => route.path === path);
   root.appendChild(createHeader());
-  root.appendChild(createHomePage());
+  if (index >= 0) root.appendChild(routes[index].content);
 } catch (e) {
   console.error(e);
 }
